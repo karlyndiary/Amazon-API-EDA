c651140eadd8c7560551ee75b5adf92bcc6c8f3c
Karen Judelyn Fernandes <116041695+karlyndiary@users.noreply.github.com>
2024-07-17 14:08:36 +0530

    Update Data Cleaning in SQL.sql

--- Data Cleaning in SQL.sql
+++ Data Cleaning in SQL.sql
@@ -5,5 +5,11 @@
 # keep the float star of the product and mean for no stars
 
 # mean value for rating 
+UPDATE [Amazon].[dbo].[Laptops]
+SET rating = (
+	SELECT AVG(CAST(rating AS float)) AS mean_rating -> replace float to int
+	FROM [Amazon].[dbo].[Laptops]
+	WHERE ISNUMERIC(rating) = 1)
+WHERE rating = 'no data'
 
 # replace no data in device setup to no device setup option available
