58e90972d9908f9eab8aad10ba6e364029a4d994
Karen Judelyn Fernandes <116041695+karlyndiary@users.noreply.github.com>
2024-07-17 14:03:02 +0530

    Update Data Cleaning in SQL.sql

--- Data Cleaning in SQL.sql
+++ Data Cleaning in SQL.sql
@@ -1,1 +1,9 @@
+# cleaning prices 
 
+# cleaning brackets from discount_percent
+
+# keep the float star of the product and mean for no stars
+
+# mean value for rating 
+
+# replace no data in device setup to no device setup option available
