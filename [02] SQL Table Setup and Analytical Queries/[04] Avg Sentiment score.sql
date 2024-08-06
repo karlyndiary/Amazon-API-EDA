e979fc7f5d9be6b243fefc1fd15153431545d6f6
Karen Judelyn Fernandes <116041695+karlyndiary@users.noreply.github.com>
2024-08-06 17:02:15 +0530

    Update [04] Avg Sentiment score.sql

--- [02] SQL Table Setup and Analytical Queries/[04] Avg Sentiment score.sql
+++ [02] SQL Table Setup and Analytical Queries/[04] Avg Sentiment score.sql
@@ -1,1 +1,6 @@
 -- Retrieve products with an average sentiment score higher than the overall average sentiment.
+
+SELECT product_asin, AVG(review_star_rating) as avg_sentiment
+FROM ReviewsView
+GROUP BY product_asin
+HAVING AVG(review_star_rating) > (SELECT AVG(review_star_rating) FROM ReviewsView);
