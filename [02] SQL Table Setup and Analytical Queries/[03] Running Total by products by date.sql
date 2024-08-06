f16cd0fbd2a59876af3e160452f6f42d28876be4
Karen Judelyn Fernandes <116041695+karlyndiary@users.noreply.github.com>
2024-08-06 15:03:14 +0530

    Update [03] Running Total by products by date.sql

--- [02] SQL Table Setup and Analytical Queries/[03] Running Total by products by date.sql
+++ [02] SQL Table Setup and Analytical Queries/[03] Running Total by products by date.sql
@@ -1,2 +1,6 @@
--- Calculate the running total of reviews for each product ordered by review date.
+-- Which product(s) have the highest number of reviews and what is the date of their most recent review?
 
+SELECT product_asin, MAX(date) AS last_review_date, COUNT(*) AS total_reviews
+FROM ReviewsView 
+GROUP BY product_asin
+ORDER BY total_reviews DESC;
