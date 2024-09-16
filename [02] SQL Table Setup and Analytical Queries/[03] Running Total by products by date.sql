f23d29dfa63306fe09c356044697bcfd918d6770
Karen Judelyn Fernandes <116041695+karlyndiary@users.noreply.github.com>
2024-09-16 09:04:24 +0530

    Delete [02] SQL Table Setup and Analytical Queries directory

--- [02] SQL Table Setup and Analytical Queries/[03] Running Total by products by date.sql
+++
@@ -1,6 +0,0 @@
--- Which product(s) have the highest number of reviews and what is the date of their most recent review?
-
-SELECT product_asin, MAX(date) AS last_review_date, COUNT(*) AS total_reviews
-FROM ReviewsView 
-GROUP BY product_asin
-ORDER BY total_reviews DESC;
